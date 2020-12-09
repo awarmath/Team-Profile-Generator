acbfd58fc234e1bd63968c90ca5085e15a9941ab
awarmath <andrew@carmunity.io>
2020-12-09 16:18:12 -0600

    working app

--- app.js
+++ app.js
@@ -1,64 +1,180 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const Manager = require('./lib/Engineer');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 const employees = [];
+
+function initApp() {
+    startHtml();
+    addEmployee();
+}
 
 function addEmployee() {
     inquirer.prompt([{
         message: "Enter new employee's name",
-        name: 'name'
+        name: "name"
     },
     {
-        type: 'list',
-        message: "Select the employee's role",
-        choices: ['Engineer', 'Intern', 'Manager'],
-        name: 'role'
+        type: "list",
+        message: "Select team new employee's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
     },
     {
-        message: "Enter new employee's ID",
-        name: 'id'
+        message: "Enter new employee's id",
+        name: "id"
     },
     {
         message: "Enter new employee's email address",
-        name: 'email'
+        name: "email"
     }])
-    thisExpression(function({name, role, id, email}) {
+    .then(function({name, role, id, email}) {
         let roleInfo = "";
-        if (role === 'Engineer') {
-            roleInfo = 'GitHub username';
-        } else if (role === 'Intern') {
-            roleInfo = 'School Name';
+        if (role === "Engineer") {
+            roleInfo = "GitHub username";
+        } else if (role === "Intern") {
+            roleInfo = "school name";
         } else {
-            roleInfo = 'Office Phone Number';
+            roleInfo = "office phone number";
         }
         inquirer.prompt([{
-            message: `Enter employee's ${roleInfo}`,
-            name: 'roleInfo'
+            message: `Enter new employee's ${roleInfo}`,
+            name: "roleInfo"
         },
         {
-            type: 'list',
-            message: 'Would you like to add more employees',
-            choices: ['yes', 'no,'],
-            name: 'moreEmployees'
+            type: "list",
+            message: "Would you like to add more new employees?",
+            choices: [
+                "yes",
+                "no"
+            ],
+            name: "moreEmployees"
         }])
         .then(function({roleInfo, moreEmployees}) {
             let newEmployee;
-            if (role === 'Engineer') {
+            if (role === "Engineer") {
                 newEmployee = new Engineer(name, id, email, roleInfo);
-            } else if (role === 'Manager') {
-                newEmployee = new Manager(name, id, email, roleInfo);
-            } else {
+            } else if (role === "Intern") {
                 newEmployee = new Intern(name, id, email, roleInfo);
+            } else {
+                newEmployee = new Manager(name, id, email, roleInfo);
             }
             employees.push(newEmployee);
-            console.log(employees);
-            if (moreEmployees === 'yes') {
-                addEmployee();
-            }
+            addHtml(newEmployee)
+            .then(function() {
+                if (moreEmployees === "yes") {
+                    addEmployee();
+                } else {
+                    finishHtml();
+                }
+            });
+            
         });
     });
 }
 
-addEmployee();
+function startHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-danger mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">My Team</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile("./output/team.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
+}
+
+function addHtml(employee) {
+    return new Promise(function(resolve, reject) {
+        const name = employee.getName();
+        const role = employee.getRole();
+        const id = employee.getId();
+        const email = employee.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const gitHub = employee.getGithub();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: <a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${gitHub}/">${gitHub}</a></li>
+            </ul>
+            </div>
+        </div>`;
+        } else if (role === "Intern") {
+            const school = employee.getSchool();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: <a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else {
+            const officePhone = employee.getOfficeNumber();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: <a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+            </ul>
+            </div>
+        </div>`
+        }
+        console.log("adding team employee");
+        fs.appendFile("./output/team.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    });
+    
+            
+    
+        
+    
+    
+}
+
+function finishHtml() {
+    const html = ` </div>
+    </div>
+    
+</body>
+</html>`;
+
+    fs.appendFile("./output/team.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("end");
+}
+
+initApp();
